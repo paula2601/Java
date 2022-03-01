866e970971e038005795b07c7c405572b0e25ff3
paula <paula@szalata.pl>
2022-03-01 12:44:24 +0100

    nowe

---
+++ Nowe.py
@@ -0,0 +1,6 @@
+seriale = { "Opowiesci podrecznej" : 8, "Vikingowie": 9.5 , "Sabrina" : 7.5 , "Lupin" : 8}
+print(list(seriale.keys()))
+print('***********')
+a = input ("Jaki serial w chciałbyś obejrzeć?")
+print ("Serial {} posiada ocenę {}".format(a, seriale[a])
+
